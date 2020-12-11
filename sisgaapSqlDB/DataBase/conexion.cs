c4c8ca086161cea0da993eefa9253f3a6532f09d
Renzo Guerra Candela <renzoguerracandela041100@gmail.com>
2020-12-11 02:26:49 -0500

    CONEXION

---
+++ sisgaapSqlDB/DataBase/conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisgaapSqlDB.DataBase
+{
+    class conexion
+    {
+        public static string CadenaConexion
+        {
+            get { return "data source= (Local); initial catalog=BD_SISGAAP; integrated security = true"; }
+        }
+
+    }
+}
